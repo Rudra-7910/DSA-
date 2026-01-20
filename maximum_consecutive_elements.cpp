0ed952ac5e76189315d2f114f4920e46e69454d3
Rudra <grudraksh825@gmail.com>
2026-01-21 00:17:08 +0530

    maximum consecutive elements

---
+++ maximum_consecutive_elements.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+int main ()
+{
+    vector<int> elements={1,1,1,6,6,3,2,1,1,1,1,1};
+    int maxi=0;
+    int cnt=0;
+    for(int i=0;i<elements.size();i++)
+    {
+        if(elements[i]==1)
+        {
+            cnt++;
+            maxi=max(cnt,maxi);
+        }
+        else{
+            cnt=0;
+        }
+    }
+    cout<<maxi;
+
+}
